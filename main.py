8482ec5d1c67b8ec6b9c1b6a4ed97abad078cb51
Rafael Lehnhart <rafael.lehnhart@iffarroupilha.edu.br>
2025-10-02 21:10:41 -0300

    feat: adicionar Dockerfile e docker-compose para configuração do ambiente fix: atualizar .gitignore e ajustar a execução do app para rodar via container

--- main.py
+++ main.py
@@ -488,5 +488,5 @@ def home():
     
     return render_template(HTML_TEMPLATE, invalid_emails=None, duplicate_users=None)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
